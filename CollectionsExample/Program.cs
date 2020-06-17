c8f6ad2c65e29c5845545792ad74759079662974
Dusten Knull <dakatk97@gmail.com>
2020-06-17 14:50:51 -0500

    Completed "lists of other types" and challenge

--- CollectionsExample/Program.cs
+++ CollectionsExample/Program.cs
@@ -1,67 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+using list_tutorial;
 
-namespace list_tutorial {
-
+namespace MainProgram {
     class Program {
 
         static void Main (string[] args) {
 
-            // A basic list example:
+            var tutorial1 = new BasicListExample ();
+            var tutorial2 = new ListsOfOtherTypes ();
 
-            // Step 1
-            var names = new List<string> { "Dusten", "Ana", "Felipe" };
-
-            foreach (var name in names) {
-                Console.WriteLine ($"Hello {name.ToUpper ()}!");
-            }
-
-            Console.WriteLine ();
-
-            // Step 2
-            names.Add ("Maria");
-            names.Add ("Bill");
-            names.Remove ("Ana");
-
-            foreach (var name in names) {
-                Console.WriteLine ($"Hello {name.ToUpper ()}!");
-            }
-
-            // Step 3
-            Console.WriteLine ($"My name is {names[0]}");
-            Console.WriteLine ($"I've added {names[2]} and {names[3]} to the list");
-
-            // Step 4
-            Console.WriteLine ($"The list has {names.Count} people in it");
-
-            // Step 5
-            var index = names.IndexOf ("Felipe");
-
-            if (index == -1) {
-                Console.WriteLine ($"When an item is not found, IndexOf returns {index}");
-            }
-
-            else {
-                Console.WriteLine ($"The name {names[index]} is at index {index}");
-            }
-
-            index = names.IndexOf ("Not Found");
-
-            if (index == -1) {
-                Console.WriteLine ($"When an item is not found, IndexOf returns {index}");
-            }
-
-            else {
-                Console.WriteLine ($"The name {names[index]} is at index {index}");
-
-            }
-
-            // Step 6
-            names.Sort ();
-
-            foreach (var name in names) {
-                Console.WriteLine ($"Hello {name.ToUpper ()}!");
-            }
+            tutorial1.run ();
+            tutorial2.run ();
+            tutorial2.challenge ();
         }
     }
 }
---
+++ CollectionsExample/list_tutorial/ListsOfOtherTypes.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace list_tutorial {
+
+    class ListsOfOtherTypes {
+
+        public void run () {
+
+            // Lists of other types:
+
+            // Step 1
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            // Step 2
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add (previous + previous2);
+
+            foreach (var item in fibonacciNumbers) {
+                Console.WriteLine (item);
+            }
+        }
+
+        public void challenge () {
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            // We already have the first two, so to generate up to 20,
+            // we only need 18 more fibonacci numbers
+            for (int i = 0; i < 18; i++) {
+
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add (previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers) {
+                Console.WriteLine (item);
+            }
+        }
+    }
+}
