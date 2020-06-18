fb658bdeb73276a1577e0d2816f7d009c2aee9c0
Dusten Knull <dakatk97@gmail.com>
2020-06-18 14:51:51 -0500

    Added protected accessibility example

---
+++ ProtectedExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProtectedExample {
+
+    class Program {
+
+        static void PrintShapeName (Shape shape) {
+            Console.WriteLine ($"{shape.Name}");
+        }
+
+        static void Main (string[] args) {
+
+            PrintShapeName (new Circle ());
+            PrintShapeName (new Square ());
+            PrintShapeName (new Shape ());
+        }
+    }
+
+    class Shape {
+
+        protected string mName;
+
+        public string Name {
+            get => mName;
+        }
+
+        public Shape () {
+            mName = "Generic Shape";
+        }
+    }
+
+    class Circle : Shape {
+
+        public Circle () {
+            mName = "Circle";
+        }
+    }
+
+    class Square : Shape {
+
+        public Square () {
+            mName = "Square";
+        }
+    }
+}
