b47a8de0270e2dd6d9f5179ba498942ce66a70ad
Dusten Knull <dakatk97@gmail.com>
2020-06-19 12:14:27 -0500

    Added async example

---
+++ Factorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Factorial {
+
+    class Program {
+
+        static void Main (string[] args) {
+            Go ();
+        }
+
+        public static void Go () {
+
+            GoAsync ();
+            Thread.Sleep (6000);
+        }
+
+        public static async void GoAsync () {
+
+            Console.WriteLine ("Starting");
+
+            var task1 = Sleep (5000);
+            var task2 = Sleep (3000);
+
+            int[] result = await Task.WhenAll (task1, task2);
+
+            Console.WriteLine ("Slept for a total of " + result.Sum () + " ms");
+        }
+
+        private async static Task<int> Sleep (int ms) {
+
+            Console.WriteLine ("Sleeping for {0} at {1}", ms, Environment.TickCount);
+
+            await Task.Delay (ms);
+
+            Console.WriteLine ("Sleeping for {0} finished at {1}", ms, Environment.TickCount);
+
+            return ms;
+        }
+    }
+}
