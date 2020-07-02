03f137269d4688040c6f096e1702abd04eebf3db
Dusten Knull <dakatk97@gmail.com>
2020-07-02 11:52:27 -0500

    Finished MVC tutorial

---
+++ ASPNET_Example/WebApplication1/WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers {
+
+    public class HelloController : Controller {
+
+        public ViewResult Hello1 () {
+            return View ("hello", "this string is the model");
+        }
+    }
+}
---
+++ ASPNET_Example/WebApplication2/WebApplication2/WebApplication2/Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data {
+
+    public class MvcMovieContext : DbContext {
+
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base (options) {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
